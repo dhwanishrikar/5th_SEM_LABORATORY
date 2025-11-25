2e223ea4503aa0ffdea044fbd970f4ed1f38ec26
𝐃𝐡𝐰𝐚𝐧𝐢 𝐒𝐡𝐫𝐢𝐤𝐚𝐫✨ <dhwanishrikar@gmail.com>
2025-11-25 20:31:01 +0530

    Update knn.py

--- MACHINE_LEARNING/PG-10/knn.py
+++ MACHINE_LEARNING/PG-10/knn.py
@@ -4,6 +4,7 @@ from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score, classification_report
+from sklearn.preprocessing import StandardScaler
 
 iris = load_iris()
 X,y = iris.data,iris.target
@@ -18,4 +19,5 @@ knn.fit(X_train,y_train)
 y_pred=knn.predict(X_test)
 accuracy=accuracy_score(y_test,y_pred)
 print('accuracy:',accuracy)
+
 print('Classification Report:\n', classification_report(y_test, y_pred))
