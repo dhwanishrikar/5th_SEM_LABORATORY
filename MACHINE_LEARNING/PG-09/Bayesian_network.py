57d17a7e8c2f6e015cb0074bedbbc265751bafcb
𝐃𝐡𝐰𝐚𝐧𝐢 𝐒𝐡𝐫𝐢𝐤𝐚𝐫✨ <dhwanishrikar@gmail.com>
2025-11-25 06:51:09 +0530

    Add files via upload

---
+++ MACHINE_LEARNING/PG-09/Bayesian_network.py
@@ -0,0 +1,21 @@
+import pandas as pd
+from pgmpy.models import BayesianModel
+from pgmpy.inference import VariableElimination
+from pgmpy.estimators import MaximumLikelihoodEstimator
+
+data=pd.read_csv('heart.csv')
+subset_data=data[['age','sex','cp','thalach','exang','oldpeak','target']]
+print(subset_data.head())
+
+model=BayesianModel([('age','thalach'),
+                     ('sex','thalach'),
+                     ('cp','thalach'),
+                     ('thalach','target'),
+                     ('exang','target'),
+                     ('oldpeak','target')
+                    ])
+model.fit(subset_data, estimator=MaximumLikelihoodEstimator)
+inference=VariableElimination(model)
+evidence={'age':55,'sex':1,'cp':3,'thalach':150,'exang':0,'oldpeak':2.3}
+result=inference.query(variables=['target'], evidence=evidence)
+print(result)
